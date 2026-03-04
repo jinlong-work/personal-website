c76bc490f366bc5fdd5871ffae833583a054aabe
caojinlong <1426559553@qq.com>
2026-03-04 15:36:48 +0800

    deploy my vue site

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/personal-website/', // 👈 重要！必须和你的 GitHub 仓库名称完全一致
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
